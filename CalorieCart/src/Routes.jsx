6ee6d351e6cd1b5165f1f928121efd20fac1823d
wmu66 <weiler.marko@gmail.com>
2024-12-29 16:53:36 +0100

    Added new welcome window

--- CalorieCart/src/Routes.jsx
+++ CalorieCart/src/Routes.jsx
@@ -6,18 +6,20 @@ import ListEditorPage from "./pages/ListEditorPage";
 import StatsPage from "./pages/StatsPage";
 import TargetSetterPage from "./pages/TargetSetterPage";
 import ImprovementPage from "./pages/ImprovementPage";
+import WelcomePage from "./pages/WelcomePage";
 
 const Routing = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/detail_entry" element={<HomePage />} />
         <Route path="/create_list" element={<ListCreatorPage />} />
         <Route path="/list_editor/:listID" element={<ListEditorPage />} />
         <Route path="*" element={<PageNotFound />} />
         <Route path="/stats/:listID" element={<StatsPage />} />
         <Route path="/targets" element={<TargetSetterPage />} />
         <Route path="/improvements/:listID" element={<ImprovementPage />} />
+        <Route path="/" element={<WelcomePage />} />
       </Routes>
     </Router>
   );
